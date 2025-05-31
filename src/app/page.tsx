d00f8e98f923d13afcf5646b5647c0647a778396
Brahim Boussada <brahimboussada@protonmail.com>
2025-05-31 23:38:05 +0300

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-9xl">Hi There Brother</h1>
+      <p>There first thing I want to say is the following: </p>
     </main>
   );
 }
