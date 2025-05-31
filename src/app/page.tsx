c31278df05e59e2c7f445517c68fe60976d3a0e5
Brahim Boussada <brahimboussada@protonmail.com>
2025-05-31 23:37:28 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-9xl">Hi There</h1>
+      <h1 className="text-9xl">Hi There Brother</h1>
     </main>
   );
 }
