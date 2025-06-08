72612190e5e17e2635374a5947cc722a3d75e57a
Brahim Boussada <brahimboussada@protonmail.com>
2025-06-08 22:38:00 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-9xl">Hi There Brother</h1>
-      <p>There first thing I want to say is the following: </p>
-      <p>The world will end one day</p>
-      <p>So be ready</p>
-      <p>I AM READY!</p>
-
-      <p>NOW</p>
+      <h1 className="text-9xl">Hi</h1>
     </main>
   );
 }
